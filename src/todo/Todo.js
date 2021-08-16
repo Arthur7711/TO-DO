bb9dac81aa535a86446b3ea02ace743a7a567c10
Arthur Ghazaryan <newartroll11@gmail.com>
2021-08-17 00:00:48 +0400

    feat:(add and change classnames fn)

--- src/todo/Todo.js
+++ src/todo/Todo.js
@@ -1,17 +1,31 @@
 import React, { useState } from "react";
+// import classNames from "classnames";
+import styles from "./Todo.module.css";
 
 export default function Todo() {
   const [value, setValue] = useState("");
   const [data, setData] = useState([]);
+  const [isSelected, setIsSelected] = useState(false);
 
   function checkValue(e) {
     setValue(e.target.value);
   }
 
   function addDodoList() {
-    setData([...data, value]);
+    setData([...data, { value: value, id: Date.now() }]);
     setValue("");
   }
+
+  const onSelectHendler = (id) => {
+    console.log("clicked", id);
+    setIsSelected(!isSelected);
+  };
+
+  const deletHendler = (id) => {
+    console.log("deleted", id);
+    const newData = data.filter((text) => text.id !== id);
+    setData(newData);
+  };
 
   return (
     <div>
@@ -22,15 +36,29 @@ export default function Todo() {
         onChange={checkValue}
       />
       <button onClick={addDodoList}>click</button>
-      <div>
-        {data &&
-          data.map((d, i) => (
-            <p key={i}>
-              {d}
-              <span>✅</span>
-              <span>❌</span>
-            </p>
-          ))}
+      <div className>
+        {data.map((d) => (
+          <p
+            style={
+              isSelected === false
+                ? { textDecoration: "none" }
+                : { textDecoration: "line-through" }
+            }
+            // className={classNames("todo", {
+            //   "todo-selected": isSelected,
+            // })}
+            key={d.id}
+          >
+            {d.value}
+            <span onClick={() => onSelectHendler(d.id)} className={styles.done}>
+              ✅
+            </span>
+
+            <span onClick={() => deletHendler(d.id)} className={styles.delete}>
+              ❌
+            </span>
+          </p>
+        ))}
       </div>
     </div>
   );
